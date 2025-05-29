0f858a37d9df9f5c438e6172cc99bbc0de4a9420
wikai <80284758+wikai@users.noreply.github.com>
2025-05-29 21:06:03 +0200

    Add StatCardSmall to DashboardHome

--- src/pages/Dashboard/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome.jsx
@@ -1,6 +1,13 @@
+import styles from './DashboardHome.module.css'
+import StatCardSmall from "../../features/dashboard/home/StatCardSmall"
+
 const DashboardHome = () => {
   return (
-    <div>DashboardHome page</div>
+    <div className={styles.topStats}>
+      <StatCardSmall icon="/icons/CalendarIcon.svg" stat="Upcoming Events" statValue='345' />
+      <StatCardSmall icon="/icons/CheckmarkIcon.svg" stat="Total bookings" statValue='1798' />
+      <StatCardSmall icon="/icons/TicketIcon.svg" stat="Tickets sold" statValue='1250' />
+    </div>
   )
 }
 export default DashboardHome
