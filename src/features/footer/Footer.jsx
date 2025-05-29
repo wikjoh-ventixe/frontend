2215926f00e8acfd46f87736c0ea59f9c5f96179
wikai <80284758+wikai@users.noreply.github.com>
2025-05-29 20:21:02 +0200

    Add footer

--- src/features/footer/Footer.jsx
+++ src/features/footer/Footer.jsx
@@ -1,6 +1,13 @@
+import styles from './Footer.module.css'
+
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className={styles.footer}>
+      <span className={styles.copyright}>Copyright © 2025 Ventixe</span>
+      <span className={styles.links}>Privacy Policy</span>
+      <span className={styles.links}>Terms and Conditions</span>
+      <span className={styles.links}>Contact</span>
+    </footer>
   )
 }
 export default Footer
