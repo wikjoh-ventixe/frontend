0ccb21dd3cf2e6421f6d7fb8e5d836473dae2fa1
wikai <80284758+wikai@users.noreply.github.com>
2025-05-29 20:12:25 +0200

    Add dashboard header

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -1,6 +1,17 @@
+import styles from './Header.module.css'
+
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className={styles.header}>
+      <div className={styles.containerLeft}>
+        <h4>Dashboard</h4>
+        <span className={styles.subText}>Hello User, welcome back!</span>
+      </div>
+      <div className={styles.containerRight}>
+        <span className={styles.username}>User Name</span>
+        <span className={styles.subText}>Admin</span>
+      </div>
+    </header>
   )
 }
 export default Header
