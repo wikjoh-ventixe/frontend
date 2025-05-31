744cc25273b2839362ae768c7af7a4cba8336926
wikai <80284758+wikai@users.noreply.github.com>
2025-05-31 13:42:39 +0200

    Add basic bookings page

--- src/pages/Dashboard/DashboardBookings.jsx
+++ src/pages/Dashboard/DashboardBookings.jsx
@@ -1,6 +1,13 @@
+import StatCardSmall from '../../features/dashboard/bookings/StatCardSmall'
+import styles from './DashboardBookings.module.css'
+
 const DashboardBookings = () => {
   return (
-    <div>DashboardBookings page</div>
+      <div className={styles.topStats}>
+      <StatCardSmall icon="/icons/TicketIcon.svg" stat="Total Bookings" statValue='55,000' />
+      <StatCardSmall icon="/icons/CheckmarkIcon.svg" stat="Total Tickets Sold" statValue='45,000' />
+      <StatCardSmall icon="/icons/DollarIcon.svg" stat="Total Earnings" statValue='$275,450' />
+    </div>
   )
 }
 export default DashboardBookings
