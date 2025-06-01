a31b7345198573525d33c18953ead6d959e45c2c
wikai <80284758+wikai@users.noreply.github.com>
2025-06-01 20:47:15 +0200

    Update header to change its contents dynamically depending on what page is selected.

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -1,12 +1,24 @@
+import { useLocation } from 'react-router-dom';
 import styles from './Header.module.css'
 
 const Header = () => {
+  const location = useLocation();
+  const pathsegments = location.pathname.split('/').filter(Boolean);
+  const lastSegment = pathsegments[pathsegments.length - 1];
+
   return (
     <header className={styles.header}>
-      <div className={styles.containerLeft}>
-        <h4>Dashboard</h4>
-        <span className={styles.subText}>Hello User, welcome back!</span>
-      </div>
+      {lastSegment !== 'home'
+      ? (<div className={styles.containerLeftDynamic}>
+          <h4>{lastSegment}</h4>
+        </div>)
+        
+      : (<div className={styles.containerLeft}>
+          <h4>Dashboard</h4>
+          <span className={styles.subText}>Hello User, welcome back!</span>
+        </div>)
+      }
+
       <div className={styles.containerRight}>
         <span className={styles.username}>User Name</span>
         <span className={styles.subText}>Admin</span>
